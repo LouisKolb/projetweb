414dd83684135040606d190293bb10dece5ad2a3
Arthur Michel <Arthur.michel1@viacesi.fr>
2019-01-17 11:46:19 +0100

    add footer

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,36 @@
+<footer class="page-footer black">
+    <div class="container">
+        <div class="row">
+            <div class="col s2 offset-s3 center-align">
+                <a class="grey-text text-lighten-3" href="#!"><i class="fab fa-facebook-square zoom"></i></a>
+            </div>
+            <div class="col s2 center-align">
+                <a class="grey-text text-lighten-3" href="#!"><i class="fab fa-instagram zoom"></i></a>
+            </div>
+            <div class="col s2 center-align">
+                <a class="grey-text text-lighten-3" href="#!"><i class="fab fa-twitter zoom"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col s2 offset-s2 center-align">
+                    <i>Mentions légales</i>
+                </div>
+                <div class="col s1 center-align">
+                    <i>|</i>
+                </div>
+                <div class="col s2 center-align">
+                    <i>Politique de confidentialité</i>
+                </div>
+                <div class="col s1 center-align">
+                    <i>|</i>
+                </div>
+                <div class="col s2 center-align">
+                    <i>© 2019 CESI</i>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
