14900cb121b0a3194ae95bcb4ea9563a9c36597c
Arthur Michel <Arthur.michel1@viacesi.fr>
2019-01-17 12:26:57 +0100

    add role system

--- app/User.php
+++ app/User.php
@@ -8,4 +8,37 @@ class user extends Model
 {
     //
     protected $fillable  = ['username','first_name','last_name','email','password'];
+
+
+
+
+
+
+
+
+        public function roles()
+        {
+            return $this->belongsToMany('App\Role','role_user');
+        }
+        public function addRole($rolename){
+            $role = role::where('name',$rolename)->first();
+            $this->roles()->attach($role->id);
+            
+        }
+        
+        
+        
+        
+        public function hasRole($rolename){
+            $roles = $this->roles;
+            foreach($roles as $role){
+                if($role->name == $rolename)
+                    return true;
+            }
+            
+        }
+
+
+
+
 }
