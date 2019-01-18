9257708cdb2c506e70f10f4165a49bf1348d67c6
unknown <sebg68000@gmail.com>
2019-01-18 14:22:23 +0100

    Add Navbar header footer + store

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,36 @@
+<footer class="page-footer black">
+    <div class="container">
+        <div class="row">
+            <div class="col m2 offset-m3 s4 center-align">
+                <a class="grey-text text-lighten-3" href="#!"><i class="fab fa-facebook-square zoom"></i></a>
+            </div>
+            <div class="col m2 s4 center-align">
+                <a class="grey-text text-lighten-3" href="#!"><i class="fab fa-instagram zoom"></i></a>
+            </div>
+            <div class="col m2 s4 center-align">
+                <a class="grey-text text-lighten-3" href="#!"><i class="fab fa-twitter zoom"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col m2 offset-m2 s4 center-align">
+                    <i>Mentions légales</i>
+                </div>
+                <div class="col m1 hide-on-small-only center-align">
+                    <i>|</i>
+                </div>
+                <div class="col m2 s4 center-align">
+                    <i>Politique de confidentialité</i>
+                </div>
+                <div class="col m1 hide-on-small-only center-align">
+                    <i>|</i>
+                </div>
+                <div class="col m2 s4 center-align">
+                    <i>© 2019 CESI</i>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
