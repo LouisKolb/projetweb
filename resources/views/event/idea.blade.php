b1f9c30a278be64c991df878e432635241342244
Guiguiprog <guillaume.doignon@viacesi.fr>
2019-01-21 14:21:26 +0100

    add dynamicly events to the view

--- resources/views/event/idea.blade.php
+++ resources/views/event/idea.blade.php
@@ -7,181 +7,6 @@
         <a href="#" class="twitter"><i class="fab fa-twitter"></i></a>
     </div>
 
-    <!-- Parralax image with border -->
-
-    <div class="parallax-container center valign-wrapper borderdown">
-        <div class="parallax"><img src="/image/background.jpg">
-        </div>
-        <div class="container white-text">
-            <div class="row">
-                <div class="col s12">
-                    <h2>Événements du mois</h2>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- <div class="row center-align"> -->
-    <section class="event-container">
-        <!-- monthly event section -->
-        <div class="row">
-            <div class="col s12">
-                <!-- User who created the event profile-->
-                <div class="card-panel grey lighten-5 z-depth-1">
-                    <div class="row">
-                        <div class="col s12 m6">
-                            <div class="row valign-wrapper">
-                                <div class="col s2 m2 l1">
-                                    <a class="black-text" href="userProfile.html">
-                                        <img class="circle responsive-img profile-pic" src="/image/profile.jpg">
-                                    </a>
-                                </div>
-                                <div class="col s3 m3 l3">
-                                    <a class="black-text" href="userProfile.html">
-                                        <p>Prénom Nom</p>
-                                    </a>
-                                </div>
-                                <div class="col s7 m7 l8 left">
-                                    <a class="black-text" href="event.html">
-                                        <h5>Titre évènement</h5>
-                                    </a>
-                                </div>
-                            </div>
-                            <hr class="divider">
-                            <!-- Event Description -->
-                            <div class="col m12">
-                                <a class="black-text" href="event.html">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse
-                                    lectus
-                                    tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum
-                                    ultrices diam.
-                                    Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor,
-                                    orci
-                                    nec
-                                    nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis
-                                    semper.
-                                    Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque
-                                    congue.
-                                    Ut in
-                                    risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue.
-                                </a>
-                            </div>
-
-                            <!-- Last comment on event display only on pc-->
-                            <div class="col m12">
-                                <div class=" card-panel grey lighten-5 z-depth-1 hide-on-med-and-down">
-                                    <div class="row">
-                                        <div class="col s1">
-                                            <a class="black-text" href="userProfile.html">
-                                                <img src="/image/profile.jpg" alt="" class="circle responsive-img">
-                                            </a>
-                                        </div>
-                                        <div class="col s11">
-                                            <div class="s12 left">
-                                                <a class="black-text" href="event.html">
-                                                    <p>Prénom Nom</p>
-                                                </a>
-                                            </div>
-                                            <div class="s12 left comment">
-                                                This is a comment : Lorem ipsum dolor sit amet, consectetur
-                                                adipiscing elit. Sed non risus. Suspendisse lectus.Lorem ipsum
-                                                dolor sit amet, consectetur
-                                                adipiscing elit. Sed non risus. Suspendisse lectus.Lorem ipsum
-                                                dolor sit amet, consectetur
-                                                adipiscing elit. Sed non risus. Suspendisse lectus.
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col s12 right-align">
-                                <a class="waves-effect waves-dark btn btn-event" href="event.html"><i class="fas fa-align-right right"></i>Voir
-                                    la publication</a>
-                            </div>
-                        </div>
-                        <!-- Event's pic in a carousel slider for beautyness -->
-                        <div class="col m6 s12">
-                            <div class="carousel carousel-slider">
-                                <a class="carousel-item event-pic"><img src="/image/event.jpg"></a>
-                                <a class="carousel-item event-pic"><img src="https://lorempixel.com/800/400/food/2"></a>
-                                <a class="carousel-item event-pic"><img src="https://lorempixel.com/800/400/food/3"></a>
-                                <a class="carousel-item event-pic"><img src="https://lorempixel.com/800/400/food/4"></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </section>
-    <!-- </div> -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     <!-- Parallax pic with border -->
     <div class="parallax-container center valign-wrapper blueborders">
         <div class="parallax"><img src="/image/background.jpg">
@@ -199,7 +24,6 @@
     <!-- Event finished -->
     <section>
         @foreach ($events as $event)
-        @if($event->statut==1)
         <div class="row">
             <div class="col s12">
                 <!-- User who created the event profile-->
@@ -209,12 +33,12 @@
                             <div class="row valign-wrapper">
                                 <div class="col s2 m2 l1">
                                     <a class="black-text" href="userProfile.html">
-                                        <img class="circle responsive-img profile-pic" src="/image/profile.jpg">
+                                        <img class="circle responsive-img profile-pic" src="{{App\user::find($event->user_id)->avatar}}">
                                     </a>
                                 </div>
                                 <div class="col s3 m3 l3">
                                     <a class="black-text" href="userProfile.html">
-                                        <p>Prénom Nom</p>
+                                        {{App\user::find($event->user_id)->first_name}} {{App\user::find($event->user_id)->last_name }}
                                     </a>
                                 </div>
                                 <div class="col s7 m7 l6">
@@ -240,45 +64,46 @@
                                     <div class="row">
                                         <div class="col s1">
                                             <a class="black-text" href="userProfile.html">
-                                                <img src="/image/profile.jpg" alt="" class="circle responsive-img">
+                                                <img src="{{App\user::find($event->user_id)->avatar}}" alt="" class="circle responsive-img">
                                             </a>
                                         </div>
                                         <div class="col s11">
-                                            <div class="s12 left">
-                                                <a class="black-text" href="event.html">
-                                                    <p>Prénom Nom</p>
-                                                </a>
-                                            </div>
                                             <div class="s12 left comment">
-                                                ceci est un commentaire commentaire
+                                              <div class="col s11">
+                                                  <div class="s12 left">
+                                                      <a class="black-text" href="event.html">
+                                                          <p>Prénom Nom</p>
+                                                      </a>
+                                                  </div>
+                                                  <div class="s12 left comment">
+                                                      This is a comment : Lorem ipsum dolor sit amet, consectetur
+                                                      adipiscing elit. Sed non risus. Suspendisse lectus.Lorem ipsum
+                                                      dolor sit amet, consectetur
+                                                      adipiscing elit. Sed non risus. Suspendisse lectus.Lorem ipsum
+                                                      dolor sit amet, consectetur
+                                                      adipiscing elit. Sed non risus. Suspendisse lectus.
+                                                  </div>
+                                              </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             <div class="col s12 right-align">
-                              @if($event->statut==0)
-                              <a href="#"></a> <form class="waves-effect waves-dark btn btn-event" action="/event/annule/{{$event->id}}" method="post">
-                              @csrf
-                              <input type="hidden" value="0" name="validate">
-                              <input type="hidden" name="_method" value="put">
-                              <input type="submit" value="Annuler">
-                              </form>
-                              @endif
-                              @if($event->statut==1)
-                              <a href="#"></a> <form class="waves-effect waves-dark btn btn-event" action="/event/annule/{{$event->id}}" method="post">
+                              @if(App\user::find($event->user_id)->hasRole('Admin'))
+                              <form class="waves-effect waves-dark btn btn-event" action="/event/valide/{{$event->id}}" method="post">
                               @csrf
                               <input type="hidden" value="1" name="validate">
                               <input type="hidden" name="_method" value="put">
                               <input type="submit" value="Valider">
                               </form>
-                              @endif
-                              <form class="waves-effect waves-dark btn btn-event" action="/event/{{$event->id}}" method="post">
+                              <form class="waves-effect waves-dark btn btn-event" action="/event/valide/{{$event->id}}" method="post">
                               @csrf
-                              <input type="hidden" name="_method" value="delete">
                               <input type="hidden" value="delete" name="validate">
-                              <input type="submit" class="" value="Supprimer">
+                              <input type="hidden" name="_method" value="put">
+                              <input type="submit" value="Supprimer">
                               </form>
+                              @endif
                                 <a class="waves-effect waves-dark btn btn-event" href="/event/{{$event}}"><i class="fas fa-align-right right"></i>
                                   Voir la publication
                                 </a>
@@ -288,38 +113,18 @@
                         <!-- Event's pic in a carousel slider for beautyness -->
                         <div class="col m6 s12">
                             <div class="carousel carousel-slider">
-                                <a class="carousel-item event-pic"><img src="/image/event.jpg"></a>
-                                <a class="carousel-item event-pic"><img src="https://lorempixel.com/800/400/food/2"></a>
-                                <a class="carousel-item event-pic"><img src="https://lorempixel.com/800/400/food/3"></a>
-                                <a class="carousel-item event-pic"><img src="https://lorempixel.com/800/400/food/4"></a>
+                               {{--@foreach ($pictures as $picture)
+                                 <a class="carousel-item event-pic"><img src="{{$picture->url}}"></a>
+                               @endforeach--}}
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        @endif
         @endforeach
     </section>
 @endsection
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 @section('scripts')
     <script>
