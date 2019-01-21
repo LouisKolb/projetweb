b6014b548dfe0772108a6e2ed69803f17b198072
Arthur Michel <Arthur.michel1@viacesi.fr>
2019-01-21 16:30:28 +0100

    add picture to events

--- app/event.php
+++ app/event.php
@@ -3,8 +3,22 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class event extends Model
 {
-    //
+    
+    public function pictures()
+    {
+        // return $this->hasMany('App\picture','event_picture');
+        return $this->belongsToMany('App\picture','event_picture');
+    }
+    public function addPicture($pictureid){
+
+        DB::table('event_picture')->insert([
+            ['event_id' => $this->id, 'picture_id' => $pictureid]
+        ]);
+
+    }
 }
+
