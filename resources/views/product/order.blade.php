34b5ca3d4848e65f988f978b0239473d916349f0
Guiguiprog <guillaume.doignon@viacesi.fr>
2019-01-22 12:55:27 +0100

    back order form

--- resources/views/product/order.blade.php
+++ resources/views/product/order.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.master') 
+@extends('layout.master')
 @section('content')
 
 <section id="section">
@@ -21,44 +21,11 @@
         <li>
             <div class="collapsible-header">
                 <h6> Votre article </h6>
-            </div>
-
-            <div class="collapsible-body">
-                <div class="row">
-                    <div class="col s6 m6 l6">
-                        <h5>Titre article</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae repellendus sunt blanditiis, eaque
-                            impedit laboriosam ab nihil obcaecati provident animi voluptatum ratione ducimus saepe asperiores
-                            quos inventore ullam. Quis, doloribus?</p>
-                        <div class="col s6 m6 l6">
-
-                            <select>
-                                    <option value="" disabled selected>Nombre d'article</option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="1">4</option>
-                                    <option value="2">5</option>
-                                    <option value="3">6</option>
-                                    <option value="3">7</option>
-                                    <option value="3">8</option>
-                                    <option value="3">9</option>
-                                    <option value="3">10</option>
-                                </select>
-                        </div>
-
-                        <div class="col s6 m6 l6">
-
-                            <button class="btn waves-effect waves-light bg-blue" type="submit" name="action">
-                                    Supprimer
-                                </button>
-                        </div>
-
-                    </div>
-                    <div class="col s6 m6 l6">
-                        <img class="img-product" src="/image/pull.png">
-                    </div>
-                </div>
+                @php
+                $product = $_GET['product_id'];
+                $quantity = $_GET['quantity'];
+                $user = $_GET['user_id'];
+                @endphp
             </div>
         </li>
         <li>
@@ -156,7 +123,7 @@
         </div>
         <div class="col s6 m6 l6">
             <button class="btn waves-effect waves-light bg-blue" type="submit" name="action">
-                Passer la commande   
+                Passer la commande
             </button>
         </div>
     </div>
@@ -168,6 +135,10 @@
         <div class="col s6 m6 l6">
             <button class="btn waves-effect waves-light bg-blue" type="submit" name="action">
                 Retour
+            </button>
+            <input id="supprimer" type="hidden" class="validate" name="supprimer" action="setcookie('panier','', time() + 365*24*3600, null, null, false, true);">
+            <button class="btn waves-effect waves-light bg-blue" type="submit" name="supprimer" >
+                Supprimer
             </button>
 
         </div>
