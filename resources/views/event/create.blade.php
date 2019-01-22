aadc81de61114cee1945b8762eb5312dbb80fdc7
simongrigoletto <simon.grigoletto@viacesi.fr>
2019-01-22 15:01:30 +0100

    Design errors messages and upgrade date picker

--- resources/views/event/create.blade.php
+++ resources/views/event/create.blade.php
@@ -28,7 +28,7 @@
             </div>
 
             <div class="input-field">
-                <input id="date" type="date" class="validate" name="date">
+                <input id="date" type="text" class="validate datepicker" name="date">
                 <label for="date">Date de l'événement</label>
             </div>
             <label>Image de présentation</label>
@@ -49,16 +49,17 @@
     </form>
 
     @if (count($errors) > 0)
-    <div class="red darken-3">
+    <div class="card-panel red lighten-5 login_waper">
         <ul>
             @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
+            <h6>
+                <li class="red-text">{{ $error }}</li>
+            </h6>
             @endforeach
         </ul>
     </div>
     @endif
 </section>
-
 @endsection
  
 @section('scripts')
@@ -71,6 +72,22 @@
 
     $(document).ready(function() {
     $('input#name, textarea#description').characterCounter();
+  });
+
+  $(document).ready(function(){
+    $('.datepicker').datepicker({
+        i18n: {
+        months: [ 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre' ],
+		monthsShort: [ 'Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jun', 'Jul', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec' ],
+		weekdays: [ 'Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi' ],
+		weekdaysShort: [ 'Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam' ],
+        weekdaysAbbrev: [ 'D', 'L', 'M', 'M', 'J', 'V', 'S' ],
+		today: 'Aujourd\'hui',
+		clear: 'Réinitialiser',
+        close: 'Fermer'
+        },
+        format: 'yyyy-mm-dd'
+    });
   });
 
 </script>
