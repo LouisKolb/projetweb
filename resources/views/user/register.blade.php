1b073fc915ec6f2b61b3ef70ee61b1b0cc9401ea
simongrigoletto <simon.grigoletto@viacesi.fr>
2019-01-23 11:48:03 +0100

    Design register view

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -1,60 +1,70 @@
 @extends('layout.master') 
 @section('content')
+<section id="section">
+	<div class="parallax-container center valign-wrapper border-down">
+		<div class="parallax"><img src="/image/info.jpg">
+		</div>
+		<div class="container white-text">
+			<div class="row">
+				<div class="col s12">
+					<h3>S'enregistrer</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<div class="container">
+	<h3 class="center-align">Vous devez être membre du cesi pour vous enregistrer</h3>
+	<div class="row">
+		<form class="col s12" method="POST" action="/user" id="register_form">
+			@csrf
+			<div class="row">
+				<div class="input-field col s12 m6">
+					<input id="first_name" type="text" class="validate" name="first_name">
+					<label for="first_name">Prénom</label>
+				</div>
+				<div class="input-field col s12 m6">
+					<input id="last_name" type="text" class="validate" name="last_name">
+					<label for="last_name">Nom</label>
+				</div>
+				<div class="input-field col s12 m6">
+					<input id="username" type="text" class="validate" minlength=5 name="username">
+					<label for="username">Nom d'utilisateur</label>
+				</div>
 
 
 
-  <div class="grey lighten-5 login_waper ">
-    
-    <h3 class="center-align">Inscription</h3>
 
-    <div class="row">
-      <form class="col s12" method="POST" action="/user" id="register_form">
-        @csrf
-        <div class="row">
-          <div class="input-field col s12 m6">
-            <input id="first_name" type="text" class="validate" name="first_name">
-            <label for="first_name">Prénom</label>
-          </div>
-          <div class="input-field col s12 m6">
-            <input id="last_name" type="text" class="validate" name="last_name">
-            <label for="last_name">Nom</label>
-          </div>
-          <div class="input-field col s12 m6">
-            <input id="username" type="text" class="validate" minlength=5 name="username">
-            <label for="username">Nom d'utilisateur</label>
-          </div>
+				<div class="input-field col s12 m6">
+					<i class="material-icons prefix">lock</i>
+					<input id="password" type="password" class="validate" name="password">
+					<label for="password">Mot de passe</label>
+				</div>
 
 
-          
+				<div class="input-field col s12 m6">
+					<i class="material-icons prefix">lock</i>
+					<input id="password_confirm" type="password" class="validate" name="password_confirm">
+					<label for="password_confirm">Confirmation</label>
+				</div>
 
-          <div class="input-field col s12 m6">
-            <i class="material-icons prefix">lock</i>
-            <input id="password" type="password" class="validate" name="password">
-            <label for="password">Mot de passe</label>
-          </div>
+				<div class="input-field col s12 m6">
+					<i class="material-icons prefix">email</i>
+					<input id="email" type="email" class="validate" name="email">
+					<label for="email">Email</label>
+				</div>
 
-
-          <div class="input-field col s12 m6">
-            <i class="material-icons prefix">lock</i>
-            <input id="password_confirm" type="password" class="validate" name="password_confirm">
-            <label for="password_confirm">Confirmation</label>
-          </div>
-
-          <div class="input-field col s12 m6">
-            <i class="material-icons prefix">email</i>
-            <input id="email" type="email" class="validate" name="email">
-            <label for="email">Email</label>
-          </div>
-
-          <div class="input-field col s12 m6">
-            <i class="material-icons prefix">email</i>
-            <input id="email_confirm" type="email" class="validate" name="email_confirm">
-            <label for="email_confirmation">Confirmation</label>
-          </div>
+				<div class="input-field col s12 m6">
+					<i class="material-icons prefix">email</i>
+					<input id="email_confirm" type="email" class="validate" name="email_confirm">
+					<label for="email_confirmation">Confirmation</label>
+				</div>
 
 
-          <div class="input-field col s12 m6">
-            <select name="centre">
+				<div class="input-field col s12 m6">
+					<select name="centre">
               <option value="" disabled selected>Choisis ton centre</option>
               
               @foreach ($centres as $centre)
@@ -64,27 +74,25 @@
               
               
             </select>
-            <label>Centre</label>
-          </div>
+					<label>Centre</label>
+				</div>
 
 
-          <div class="input-field col s12">
-            <button class="btn waves-effect waves-light" id="submit" type="submit" name="submit">S'inscrire</button>
-          </div>
+				<div class="input-field col s12">
+					<button class="btn waves-effect waves-light" id="submit" type="submit" name="submit">S'inscrire</button>
+				</div>
 
-        </div>
-      </form>
-    </div>
+			</div>
+		</form>
+	</div>
 
-  </div>
-
+</div>
 @endsection
  
 @section('scripts')
 
 <script>
-
-$(document).ready(function(){
+	$(document).ready(function(){
     $('select').formSelect();
   });
       
