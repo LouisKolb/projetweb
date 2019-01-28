19a66c9712e5b726211a1b90b5ccfba035e779c4
Arthur Michel <Arthur.michel1@viacesi.fr>
2019-01-28 22:19:33 +0100

    Add top articles in welcome

--- app/product.php
+++ app/product.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class product extends Model
 {
+
+  
+
+
+
+
     public function picture(){
         return $this->hasOne('App\Picture','id');
     }
@@ -20,4 +26,50 @@ class product extends Model
         }
         return $cheat;
     }
+
+    public function orders(){
+        return $this->belongsToMany('App\order','product_order','product_id','order_id');
+    }
+    public function countCommanded(){
+        $counter = 0;
+        foreach ($this->orders as $o) {
+            $counter++;
+        }
+        return $counter;
+    }
+
+    
+    public static function mostCommanded(){
+        $products = product::get();
+        $bests = product::bubbleSort($products);
+        $count = count($bests)<3?count($bests)-1:3;
+        $temp = array();
+        foreach ($bests as $p) {
+            array_push($temp,$p);
+        }
+
+        return array_slice($temp,0,$count);
+        return $temp;
+    }
+
+    public static function bubbleSort($array){
+        $size = count($array)-1;
+        for ($i=0 ; $i < $size-1 ; $i++) {
+            for ($j=0; $j < $size-$i; $j++) { 
+                $k=$j+1;
+                if($array[$k]->countCommanded()>$array[$j]->countCommanded()){
+                    list($array[$j], $array[$k]) = array($array[$k], $array[$j]);
+                }
+            }
+        }
+        return $array;
+
+
+
+
+    }
+
+    
+
+
 }
